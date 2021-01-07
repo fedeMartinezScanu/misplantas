f02e0ac854c1aafb832c11dcdc2ef6b01fc70c85
fedeMartinezScanu <fede_martinez94@hotmail.com>
2021-01-07 01:28:42 -0300

    cambios finales

---
+++ main.js
@@ -0,0 +1,17 @@
+class Pedido {
+
+    constructor(planta, precio, cantidad){
+    this.planta = planta;
+    this.precio = precio;
+    this.cantidad = cantidad;
+}
+
+    compraRealizada(){
+
+    }
+
+    fechaComprada(){
+        
+    }
+
+}
