24dc52a1065ae151c82dc4ae6372943f3dc3118a
fedeMartinezScanu <fede_martinez94@hotmail.com>
2021-02-01 14:02:28 -0300

    Segunda entrega

--- data.js
+++ data.js
@@ -1,53 +1,62 @@
 const products = [{
-    "name" : "suculenta",
+    "id" : "A000",
+    "name" : "SUCULENTA",
     "description" : "Son aquellas en las que algún órgano está especializado en el almacenamiento de agua en cantidades mayores que las plantas sin esta adaptación. Estos órganos de reserva tienen una alta proporción de tejido parenquimático.",
     "price" : 800,
     "available" : true,
     "img" : "img/suculenta2.jpg"
 },{
-    "name" : "potus",
+    "id" : "B001",
+    "name" : "POTUS",
     "description" : "Comúnmente conocido como potus, pothos o potos es una especie de la familia Araceae nativa del sudeste asiático y Nueva Guinea. En ocasiones es confundida con philodendron en las floristerías.",
     "price" : 1000,
     "available" : true,
     "img" : "img/potus2.jpg"
 },{
-    "name" : "sansevieria",
+    "id" : "C002",
+    "name" : "SANSEVIERIA",
     "description" : "Ahora se la ha incluido en el género Dracaena debido a los estudios moleculares de su filogenia​​​. Tradicionalmente este género ha sido incluido dentro de una definición amplia de la familia Liliaceae.",
     "price" : 1100,
     "available" : true,
     "img" : "img/sansevieria.jpg"
 },{
-    "name" : "costilladeadan",
+    "id" : "D003",
+    "name" : "COSTILLA DE ADÁN",
     "description" : "Monstera deliciosa, llamada comúnmente cerimán o costilla de Adán, es una especie de planta trepadora, endémica de selvas tropicales, que se distribuye desde el centro y sur de México hasta el norte de Argentina.",
     "price" : 2500,
     "available" : true,
     "img" : "img/costilladeadan.jpg"
 },{
-    "name" : "ficus",
+    "id" : "E004",
+    "name" : "FICUS",
     "description" : "Llamado comúnmente árbol del caucho, ​ o gomero, es una especie perennifolia del género Ficus, nativa del nordeste de India, oeste de Indonesia. Fue introducida en Europa en 1815 como planta de interior.",
     "price" : 1500,
     "available" : true,
     "img" : "img/ficuselastica.jpg"
 },{
-    "name" : "lazodeamor",
+    "id" : "F005",
+    "name" : "LAZO DE AMOR",
     "description" : "Es una especie fanerógama del género Chlorophytum, nativa de Sudáfrica. Especie acaule de gruesas raíces tuberosas, forma una roseta central de hojas angostas y largas de entre 20–40 cm de longitud y 5–20 mm de ancho.",
     "price" : 700,
     "available" : true,
     "img" : "img/lazodeamor.jpg"
 },{
-    "name" : "dracena",
+    "id" : "G006",
+    "name" : "DRACENA",
     "description" : "Es una planta originaria de África tropical y Asia, no soporta el sol directo, aunque necesita de mucha luminosidad, se debe evitar los cambios bruscos de temperatura, y se debe rociar el follaje con agua de vez en cuando. Se distingue por tener en sus hojas, rayas blancas cercas de los bordes.",
     "price" : 1400,
     "available" : true,
     "img" : "img/dracena.jpg"
 },{
-    "name" : "cortadum",
+    "id" : "H007",
+    "name" : "CORDATUM",
     "description" : "Es originaria de las selvas tropicales de américa del sur, donde crece anclada a los árboles. Existen cerca de 300 especies, todas se caracterizan por tener hojas grandes y brillantes, y sus raíces crecen como nudos.",
     "price" : 900,
     "available" : true,
-    "img" : "img/cortadum.jpg"
+    "img" : "img/cordatum.jpg"
 },{
-    "name" : "helecho",
+    "id" : "I008",
+    "name" : "HELECHO",
     "description" : "El Helecho nido o Nido de ave aguanta bastante bien en habitaciones y rincones en los que llega poca luz. Requiere mucha humedad ambiental. Deberás pulverizar sus hojas a menudo, o poner la maceta sobre un plato con guijarros húmedos.",
     "price" : 750,
     "available" : true,
--- domBuilder.js
+++ domBuilder.js
@@ -3,21 +3,24 @@ class DOMBuilder {
 
     }
 
-    h3(title){
+    h3(title, className){
         const h3 = document.createElement('h3');
         h3.textContent =  title;
+        h3.classList.add(className);
         return h3;
     }
 
-    img(src){
+    img(src, className){
         const img = document.createElement('img');
         img.src = src;
+        img.classList.add(className);
         return img;
     }
 
-    p(content){
+    p(content, className){
         const p = document.createElement('p');
         p.textContent =  content;
+        p.classList.add(className);
         return p;
     }
 
@@ -27,10 +30,11 @@ class DOMBuilder {
         return h2;
     }
 
-    button(content, className){
+    button(content, className, id){
         const button = document.createElement('button');
         button.textContent =  content;
         button.classList.add(className);
+        button.setAttribute('data-id', id);
         return button;
     }
 }
